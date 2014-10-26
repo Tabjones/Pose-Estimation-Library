09b46bdff66930dae80bb2cb5299573206d93604
Federico Spinelli <fspinelli@gmail.com>
2014-10-26 16:08:32 +0100

    Initiated class interface
    new interface.h .hpp, removed elab_test.cpp (was used only in thesis)
    Removed headers from pose_estimator.cpp (tmp solution)

--- interface.h
+++ interface.h
@@ -1,5 +1,116 @@
-#include "interface.hpp"
-//PointCloud<FPFHSignature33>::Ptr fpfh (new PointCloud<FPFHSignature33>); //cloud to store the fpfhs during GFPFH pipeline
+#ifndef __INTERFACE_H_INCLUDED__
+#define __INTERFACE_H_INCLUDED__
+
+#include <iostream>
+//#include <pcl/console/parse.h>
+//#include <pcl/console/print.h>
+//#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+//#include <pcl/common/time.h>
+//#include <pcl/common/norms.h>
+//#include <pcl/surface/mls.h>
+//#include <pcl/search/kdtree.h>
+//#include <pcl/features/normal_3d_omp.h>
+//#include <pcl/filters/statistical_outlier_removal.h>
+//#include <pcl/filters/approximate_voxel_grid.h>
+//#include <pcl/features/vfh.h>
+//#include <pcl/features/esf.h>
+//#include <pcl/features/fpfh_omp.h>
+//#include <pcl/features/cvfh.h>
+//#include <pcl/features/gfpfh.h>
+//#include <pcl/features/our_cvfh.h>
+//#include <pcl/filters/voxel_grid.h>
+//#include <pcl/registration/icp.h>
+//#include <pcl/registration/ndt.h>
+//#include <pcl/visualization/pcl_visualizer.h>
+//#include <pcl/visualization/histogram_visualizer.h>
+//#include <pcl/segmentation/conditional_euclidean_clustering.h>
+//#include <flann/flann.h>
+//#include <flann/io/hdf5.h>
+//#include <boost/filesystem.hpp>
+//#include <boost/algorithm/string/split.hpp>
+//#include <boost/algorithm/string/trim.hpp>
+//#include <algorithm>
+#include <string>
+//#include <fstream>
+//#include <cmath>
+//#include <pcl/common/centroid.h>
+
+//#define D2R 0.017453293 //degrees to radians conversion
+
+using namespace pcl;
+using namespace pcl::io;
+//using namespace pcl::console;
+//using namespace boost;
+using namespace std;
+
+class Object {
+  
+  protected:
+    string name;
+    PointCloud<PointXYZRGBA> cloud;
+  
+  public:
+    Object ();
+    Object (string& str, PointCloud<PointXYZRGBA>& cl) : name(str) { copyPointCloud(cl, cloud); } 
+    void setName (string& str);
+    void setCloud (PointCloud<PointXYZRGBA>& cl);
+    void getName (string& str);
+    void getCloud (PointCloud<PointXYZRGBA>& cl);
+};
+
+class Candidate: public Object {
+  
+  protected:
+    float rank;
+    float distance;
+    float rmse;
+    Eigne::Matrix4f transformation;
+
+  public:
+    Candidate ();
+    Candidate (string& str, PointCloud<PointXYZRGBA>& cl) : name (str) { 
+      copyPointCloud(cl, cloud);
+      rank = 0;
+      distance =0;
+      rmse = 0;
+      transformation.setIdentity();
+    }
+    void setRank (float r);
+    void getRank (float& r);
+    void setDistance (float d);
+    void getDistance (float& d);
+    void setRMSE (float r);
+    void getRMSE (float& r);
+    void getTransformation (Eigen::Matrix4f& t);
+    void setTransformation (Eigen::Matrix4f& t);
+};
+
+class PoseEstimation {
+  
+  bool use_VFH, use_ESF, use_CVFH, use_OURCVFH;
+  Object query;
+  vector<Candidate> VFH_list, ESF_list, CVFH_list OURCVFH_list, composite_list;
+  int k, howmany;
+  bool upsampling, downsampling, filter, progressive;
+
+  public:
+  PoseEstimation() { 
+    use_VFH = use_ESF = use_CVFH = use_OURCVFH = downsampling = progressive = true; 
+    upsampling = filter = false;
+    k = 20;
+    howmany = 4;
+  }
+  void setQuery (Object& q) {
+    query.setName( q.name );
+    query.setCloud (q.cloud);
+  }
+  
+};
+
+    
+
 
 
 struct candidate 
--- interface.hpp
+++ interface.hpp
@@ -1,5 +1,84 @@
-#include "interface.hpp"
-//PointCloud<FPFHSignature33>::Ptr fpfh (new PointCloud<FPFHSignature33>); //cloud to store the fpfhs during GFPFH pipeline
+#ifndef __INTERFACE_HPP_INCLUDED__
+#define __INTERFACE_HPP_INCLUDED__
+
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/common/norms.h>
+#include <pcl/surface/mls.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/features/vfh.h>
+#include <pcl/features/esf.h>
+#include <pcl/features/cvfh.h>
+#include <pcl/features/our_cvfh.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/registration/icp.h>
+#include <pcl/registration/ndt.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <flann/flann.h>
+#include <flann/io/hdf5.h>
+#include <boost/filesystem.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <cmath>
+#include <pcl/common/centroid.h>
+
+#include "interface.h"
+
+#define D2R 0.017453293 //degrees to radians conversion
+
+using namespace pcl;
+using namespace pcl::io;
+using namespace pcl::console;
+using namespace boost;
+using namespace std;
+
+void Object::setName (string& str) { 
+  name = str; 
+}
+void Object::setCloud (PointCloud<PointXYZRGBA>& cl) { 
+  copyPointCloud (cl, cloud);
+}
+void Object::getName (string& str) { 
+  str = name; 
+}
+void Object::getCloud (PointCloud<PointXYZRGBA>& cl) { 
+  copyPointCloud( cloud, cl); 
+}
+void Candidate::setRank (float r) { 
+  rank = r; 
+}
+void Candidate::getRank (float& r) { 
+  r = rank; 
+}
+void Candidate::setDistance (float d) { 
+  distance = d; 
+}
+void Candidate::getDistance (float& d) { 
+  d = distance; 
+}
+void Candidate::setRMSE (float r) { 
+  rmse = r; 
+}
+void Candidate::getRMSE (float& r) { 
+  r = rmse; 
+}
+void Candidate::getTransformation (Eigen::Matrix4f& t) { 
+  t = transformation; 
+}
+void Candidate::setTransformation (Eigen::Matrix4f& t) { 
+  transformation = t; 
+}
+
+
+    
+
 
 
 struct candidate 
