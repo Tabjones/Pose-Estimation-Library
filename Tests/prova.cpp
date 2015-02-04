2ca5d5aa164fe88558acbf2a6022191a331c3f87
Federico Spinelli <fspinelli@gmail.com>
2015-02-04 20:26:41 +0100

    small test changes

--- Tests/prova.cpp
+++ Tests/prova.cpp
@@ -8,23 +8,19 @@ main (int argc, char *argv[])
 {
   printVersion();
   PointCloud<PointXYZRGBA> cloud;
-  boost::filesystem::path db_path ("");
+  boost::filesystem::path db_path ("/home/tabjones/ObjectDB/DB_Round1");
   pcl::io::loadPCDFile(argv[1], cloud);
   PoseEstimation prova;
   prova.setParam("verbosity", 2);
   prova.setParam("computeViewpointFromName",0);
   prova.setParam("useSOasViewpoint",1);
   prova.setParam("progItera",10);
-  prova.setDatabase(db_path);
-  //test.create("/home/pacman/Dropbox/ObjectDB/Round1", prova.getParams() );
-  //test.save("/media/pacman/storage/PointClouds/Database_Round1");
-  test.load("/media/pacman/storage/PointClouds/Database_Round1");
-  prova.estimate("object_23_50", cloud, test);
+  //test.create("/home/tabjones/ObjectDB/Round1", prova.getParams() );
+  //test.save("/home/tabjones/ObjectDB/DB_Round1");
+  //test.load("/media/pacman/storage/PointClouds/Database_Round1");
+  prova.estimate("object_23_50", cloud, db_path);
   prova.printCandidates();
   prova.printEstimation();
   prova.viewEstimation();
-  //prova.saveEstimation("Results/prova.estimation");
-  //prova.saveParams("provaconf");
-  //prova.saveCandidates("Results/cand.list");
   return 1;
 }
