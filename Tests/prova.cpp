fecc37055d5bf1e27116bfd90363b974cbb16317
Federico Spinelli <fspinelli@gmail.com>
2015-02-04 17:33:05 +0100

    started test

--- Tests/prova.cpp
+++ Tests/prova.cpp
@@ -8,13 +8,14 @@ main (int argc, char *argv[])
 {
   printVersion();
   PointCloud<PointXYZRGBA> cloud;
+  boost::filesystem::path db_path ("");
   pcl::io::loadPCDFile(argv[1], cloud);
   PoseEstimation prova;
   prova.setParam("verbosity", 2);
-  prova.setParam("computeViewpointFromName",1);
-  prova.setParam("useSOasViewpoint",0);
+  prova.setParam("computeViewpointFromName",0);
+  prova.setParam("useSOasViewpoint",1);
   prova.setParam("progItera",10);
-  PoseDB test;
+  prova.setDatabase(db_path);
   //test.create("/home/pacman/Dropbox/ObjectDB/Round1", prova.getParams() );
   //test.save("/media/pacman/storage/PointClouds/Database_Round1");
   test.load("/media/pacman/storage/PointClouds/Database_Round1");
